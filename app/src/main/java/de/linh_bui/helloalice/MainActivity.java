ed65b649ccb83c8b8688c308bf8b634db2c55555
Linh Bui <linhbui91@hotmail.de>
2015-07-23 18:34:24 +0200

    Commit 3
    Implementation of Text-To-Speech and Speech-To-Text function

--- app/src/main/java/de/linh_bui/helloalice/MainActivity.java
+++ app/src/main/java/de/linh_bui/helloalice/MainActivity.java
@@ -1,43 +1,59 @@
 package de.linh_bui.helloalice;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.alicebot.ab.Bot;
 import org.alicebot.ab.Chat;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Locale;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements TextToSpeech.OnInitListener{
+    private Bot alice;
+    private Chat chatSession;
+    private ImageButton btnSpeak;
+    private String botName = "alice2";
+    private String path;
+    private TextToSpeech tts;
+    private TextView txtSpeechInput;
+    private TextView txtSpeechOutput;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        extractZipFile();
 
-        File fileExt = new File(getExternalFilesDir(null).getAbsolutePath()+"/bots");
+        path = getExternalFilesDir(null).getAbsolutePath();
+        alice = new Bot(botName,path);
 
-        if(!fileExt.exists())
-        {
-            ZipFileExtraction extract = new ZipFileExtraction();
+        chatSession = new Chat(alice);
 
-            try
-            {
-                extract.unZipIt(getAssets().open("bots.zip"), getExternalFilesDir(null).getAbsolutePath()+"/");
-            } catch (Exception e) { e.printStackTrace(); }
-        }
-
-        String botname = "alice2";
-        String path = getExternalFilesDir(null).getAbsolutePath();
-        Bot alice = new Bot(botname,path);
-
-        Chat chatSession = new Chat(alice);
-
-        String request = "Hello.  Are you alive?  What is your name?";
-        String response = chatSession.multisentenceRespond(request);
-        System.out.println(response);
+        tts = new TextToSpeech(this, this);
+        txtSpeechInput = (TextView) findViewById(R.id.txtSpeechInput);
+        btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleSpeechInput();
+            }
+        });
     }
 
     @Override
@@ -60,5 +76,98 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDestroy() {
+        // Destroy tts
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onInit(int status) {
+
+        if (status == TextToSpeech.SUCCESS) {
+            int result = tts.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Intent installIntent = new Intent();
+                installIntent.setAction(
+                        TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installIntent);
+                Log.e("TTS", "This Language is not supported");
+            } else {
+            }
+
+        } else {
+            Log.e("TTS", "Initialization Failed!");
+        }
+
+    }
+
+    private void googleSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtSpeechInput.setText(result.get(0));
+                    //String response = chatSession.multisentenceRespond(result.get(0));
+                    googleSpeechOutput(result.get(0));
+                    //txtSpeechOutput.setText(response);
+                    //String request = "Hello.  Are you alive?  What is your name?";
+                }
+                break;
+            }
+
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void googleSpeechOutput(String text) {
+        CharSequence output = text;
+        tts.speak(output, TextToSpeech.QUEUE_FLUSH, null, null);
+    }
+
+    private void extractZipFile(){
+        File fileExt = new File(getExternalFilesDir(null).getAbsolutePath()+"/bots");
+
+        if(!fileExt.exists())
+        {
+            ZipFileExtraction extract = new ZipFileExtraction();
+
+            try
+            {
+                extract.unZipIt(getAssets().open("bots.zip"), getExternalFilesDir(null).getAbsolutePath()+"/");
+            } catch (Exception e) { e.printStackTrace(); }
+        }
     }
 }
