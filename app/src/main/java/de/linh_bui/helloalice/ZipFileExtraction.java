903e620e2512a0e708d0b6d64c34d3b27eabab8f
Linh Bui <linhbui91@hotmail.de>
2015-07-23 14:39:35 +0200

    First commit
    Add all files for project ALICE

---
+++ app/src/main/java/de/linh_bui/helloalice/ZipFileExtraction.java
@@ -0,0 +1,47 @@
+package de.linh_bui.helloalice;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+/**
+ * Created by Linh on 23.07.15.
+ *
+ * Extract ZIP file containing all AIML files
+ */
+public class ZipFileExtraction
+{
+    public void unZipIt(InputStream zipFile, String outputFolder)
+    {
+        try
+        {
+            //get the zip file content
+            ZipInputStream zin = new ZipInputStream(zipFile);
+            //get the zipped file list entry
+            ZipEntry entry;
+            int bytesRead;
+            byte[] buffer = new byte[4096];
+
+            while ((entry = zin.getNextEntry()) != null) {
+                if (entry.isDirectory()) {
+                    File dir = new File(outputFolder, entry.getName());
+                    if (!dir.exists()) {
+                        dir.mkdir();
+                    }
+                } else {
+                    FileOutputStream fos = new FileOutputStream(outputFolder + entry.getName());
+                    while ((bytesRead = zin.read(buffer)) != -1) {
+                        fos.write(buffer, 0, bytesRead);
+                    }
+                    fos.close();
+                }
+            }
+            zin.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
